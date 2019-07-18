9dcfb53646e9ff6caf2ae677615b95237058d14c
Omar <omar.shaban@rutgers.edu>
2019-07-18 13:34:10 -0400

    Added production startup code

--- startup/db.js
+++ startup/db.js
@@ -7,18 +7,28 @@ function dbConnect() {
     //Print the environment system is running on:
     console.log(`Running on ${process.env.NODE_ENV} environment`);
 
-    let DB = process.env.DB_DEV; //Automatically load development database
+    let uri; //Stores database uri
 
-    //If environment is set to test, load test database
-    if(process.env.NODE_ENV==="test") {
-        DB=process.env.DB_TEST
-    } else if(process.env.NODE_ENV==="production") {
-        DB=process.env.DB_PROD
+    //Connection options:
+    let options = {
+        useNewUrlParser: true
     }
 
+    //Checks if application is running in prod, test or dev:
+    if(process.env.NODE_ENV === 'production') {
+        uri = process.env.DB_PROD; //Use production database
+        options.dbName = process.env.DB_NAME; //Add MongoDB Atlas database name
+    } else if(process.env.NODE_ENV === 'test') {
+        uri = process.env.DB_HOST_TEST; //Use test database
+    } else {
+        uri = process.env.DB_HOST_DEV; //Loads development database
+    }
+
+    console.log(uri);
+
     //Connects to local MongoDB database
-    mongoose.connect(DB, {useNewUrlParser: true})
-    .then(() => console.log(`Connected to ${DB}`) )
+    mongoose.connect(uri, options)
+    .then(() => console.log(`Connected to ${uri}`) )
     .catch((error) => {
         console.log('Could not connect to MongoDB ' + error)
         process.exit(1);
